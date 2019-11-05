db9b72ff9d12d665d07403085fc2b0221fcce317
MadisonTomaso <55721314+MadisonTomaso@users.noreply.github.com>
2019-11-04 19:19:10 -0500

    Add files via upload

---
+++ MadisonTomaso-a1.py
@@ -0,0 +1,12 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> candy1="KitKat"
+candy2="Crunch Bar"
+candy3="Skittles"
+timeofday = input("Enter the time of day (M,A,E): ")
+if(timeofday==M):
+    print(candy1)
+elif(timeofday==A):
+    print(candy2)
+else:
+    print(candy3)
